6cfdab158bf4437e4165e09a7c84952a70d9e0c3
akashm01github <akashmukhopadhayay@gmail.com>
2026-07-23 13:48:23 +0530

    Day-2 of ReactJS

---
+++ Day-2/chortProject/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { About } from './About'
+const App = () => {
+  return (
+    <div>
+      <h1>Hey...</h1>
+      <About/>
+      {/* {About()} */}
+    </div>
+  )
+}
+
+export default App
